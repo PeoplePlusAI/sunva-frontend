5eea683aaa033e08f8d38f3a74792924d05a94b8
fbn776 <fbn776@gmail.com>
2024-09-25 00:07:30 +0530

    minor: added delete convo

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -18,13 +18,93 @@ import useSunvaAI from "@/lib/hooks/useSunvaAI";
 import {toast} from "sonner";
 import TTS from "@/components/tts/TTS";
 import ChangeLangBtn from "@/components/ChangeLangBtn";
-import {useSession} from "@/lib/context/sessionContext";
+import {TSessionCtx, useSession} from "@/lib/context/sessionContext";
 import {redirect} from "next/navigation";
+import {StateSetter} from "@/lib/types";
+
+
+function SaveAndDeletePrompts({isDelOpen, setIsDelOpen, session}: {
+    isDelOpen: boolean,
+    setIsDelOpen: StateSetter<boolean>,
+    session: TSessionCtx
+}) {
+    const [isDelConfoOpen, setIsDelConfoOpen] = useState(false);
+    const [isSaveOpen, setIsSaveOpen] = useState(false);
+
+    const saveNameRef = useRef<HTMLInputElement>(null);
+    return <>
+        <Dialog
+            title="Do you want to delete this conversation?"
+            acceptText="Delete"
+            rejectText="Cancel"
+            open={isDelConfoOpen}
+            setOpen={setIsDelConfoOpen}
+            onAccept={() => {
+                alert("Coming soon...")
+            }}
+
+            onReject={() => {
+                setIsDelOpen(false);
+                setIsDelConfoOpen(false);
+            }}
+        >
+            <br/>
+        </Dialog>
+
+        <Dialog
+            title="Do you want to save this conversation?"
+            acceptText="Save"
+            rejectText="Cancel"
+            open={isDelOpen}
+            setOpen={setIsDelOpen}
+            onAccept={() => {
+                setIsDelOpen(false);
+                setIsSaveOpen(true);
+            }}
+
+            onReject={() => {
+                setIsDelOpen(false);
+                setIsDelConfoOpen(true);
+            }}
+        >
+            <br/>
+        </Dialog>
+
+        <Dialog
+            title="Save As"
+            acceptText="Save"
+            rejectText="Cancel"
+            open={isSaveOpen}
+            setOpen={setIsSaveOpen}
+            onAccept={() => {
+                if (!saveNameRef.current?.value) {
+                    toast.error("Please enter a name");
+                } else {
+                    let endpoint = `api/v1/transcription/save?user_id=${encodeURIComponent(session!.user_id!)}`;
+
+                    fetch(endpoint, {
+                        method: "POST"
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data);
+                        })
+                        .catch(e => {
+                            toast.error("Couldn't save the conversation");
+                            console.error("Error while saving;", e);
+                        })
+                }
+            }}
+        >
+            <input type="text" ref={saveNameRef}
+                   className="my-5 w-full px-2 py-2 rounded-xl border-2 border-brand-secondary"
+                   placeholder="Enter here"/>
+        </Dialog>
+    </>
+}
 
 export default function Home() {
     const [session] = useSession();
-    const [isDelOpen, setIsDelOpen] = useState(false);
-    const [isSaveOpen, setIsSaveOpen] = useState(false);
     const {
         messages,
         setMessages,
@@ -34,8 +114,8 @@ export default function Home() {
         startRecording,
         stopRecording
     } = useSunvaAI(session);
-    const saveNameRef = useRef<HTMLInputElement>(null);
     const [isTTSOpen, setIsTTSOpen] = useState(false);
+    const [isDelOpen, setIsDelOpen] = useState(false);
     const ttsClose = () => {
         setIsTTSOpen(false);
     };
@@ -98,47 +178,6 @@ export default function Home() {
                 <UpAndDownArrow/>
             </div>}
 
-        <Dialog
-            title="Do you want to save this conversation?"
-            acceptText="Save"
-            rejectText="Cancel"
-            open={isDelOpen}
-            setOpen={setIsDelOpen}
-            onAccept={() => {
-                setIsDelOpen(false);
-                setIsSaveOpen(true);
-            }}
-        >
-            <br/>
-        </Dialog>
-
-        <Dialog
-            title="Save As"
-            acceptText="Save"
-            rejectText="Cancel"
-            open={isSaveOpen}
-            setOpen={setIsSaveOpen}
-            onAccept={() => {
-                if (!saveNameRef.current?.value) {
-                    toast.error("Please enter a name");
-                } else {
-                    // fetch("api/v1/transcription/save", {
-                    //     method: "POST"
-                    // })
-                    //     .then(res => res.json())
-                    //     .then(data => {
-                    //         console.log(data);
-                    //     })
-                    //     .catch(e => {
-                    //         toast.error("Couldn't save the conversation");
-                    //         console.error("Error while saving;", e);
-                    //     })
-                }
-            }}
-        >
-            <input type="text" ref={saveNameRef}
-                   className="my-5 w-full px-2 py-2 rounded-xl border-2 border-brand-secondary"
-                   placeholder="Enter here"/>
-        </Dialog>
+        <SaveAndDeletePrompts isDelOpen={isDelOpen} setIsDelOpen={setIsDelOpen} session={session}/>
     </main>
 }
