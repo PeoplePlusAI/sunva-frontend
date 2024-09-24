df04d3e64518a97b19cbd79edaa679a95660c031
fbn776 <fbn776@gmail.com>
2024-09-25 00:07:12 +0530

    feat: ribbon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,15 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
     return (
         <html lang="en">
         <body className={inter.className}>
+
+        <a
+            className="banner right-top"
+            href="https://github.com/PeoplePlusAI/sunva-ai"
+            data-ribbon="Fork Me"
+            title="Made with ❤️ for 🇮🇳 by People+AI"
+        />
+
+
         <SessionProvider>
             {children}
         </SessionProvider>
