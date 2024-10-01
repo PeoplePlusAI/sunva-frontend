2d101960fc9575ed773e29b32be02453ef31e3a4
fbn776 <fbn776@gmail.com>
2024-10-01 23:55:58 +0530

    feat: login fix

--- src/components/user/Login.tsx
+++ src/components/user/Login.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import "./user.css";
 import {useState} from "react";
 import {toast} from "sonner";
@@ -13,7 +15,44 @@ export default function Login({pageSetter}: { pageSetter: (val: TPages) => void 
     const router = useRouter();
 
     return <section className="w-full h-full flex items-center justify-center px-8">
-        <form className="flex flex-col items-center justify-center login-form gap-4 w-full">
+        <form className="flex flex-col items-center justify-center login-form gap-4 w-full" onSubmit={(e) => {
+            e.preventDefault();
+
+            fetch(`${process.env.NEXT_PUBLIC_BACKEND}/user/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                'credentials': 'include',
+                body: JSON.stringify({
+                    "email": email,
+                    "password": password
+                })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(router);
+
+                    if(data.detail) {
+                        toast.error(data.detail);
+                        return;
+                    }
+
+                    setSession({
+                        email: data.email as string,
+                        lang: data.language as string,
+                        user_id: data.user_id as string
+                    })
+                        setTimeout(() => {
+                            console.log("Done")
+                            router.push("/home");
+                        }, 1000);
+                    })
+                    .catch(e => {
+                        console.error(e);
+                        toast.error("Couldn't create the account");
+                    })
+        }}>
             <div className="unit">
                 <label htmlFor="email" className="block">Email</label>
                 <input type="email" name="email" placeholder="Enter here" className="border-[2px] rounded-lg w-full"
@@ -25,39 +64,7 @@ export default function Login({pageSetter}: { pageSetter: (val: TPages) => void 
                 <label htmlFor="password" className="block">Password</label>
                 <PasswordInput password={password} setPassword={setPassword}/>
             </div>
-            <button type="submit" className="mt-5 btn-primary px-10" onClick={(e) => {
-                e.preventDefault();
-
-                fetch(`${process.env.NEXT_PUBLIC_BACKEND}/user/login`, {
-                    method: 'POST',
-                    headers: {'Content-Type': ''},
-                    body: JSON.stringify({
-                        "email": email,
-                        "password": password,
-                    })
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data);
-
-                        if(data.detail) {
-                            toast.error(data.detail);
-                            return;
-                        }
-
-                        setSession({
-                            email: data.email as string,
-                            lang: data.language as string,
-                            user_id: data.user_id as string
-                        })
-
-                        router.push("/home");
-                    })
-                    .catch(e => {
-                        console.error(e);
-                        toast.error("Couldn't create the account");
-                    })
-            }}>
+            <button type="submit" className="mt-5 btn-primary px-10">
                 Login
             </button>
 
